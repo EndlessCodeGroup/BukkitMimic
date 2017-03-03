42986a656ac8babb7846943e1a367c9c8df8e7c3
OsipXD <osip.fatkullin@gmail.com>
2017-03-04 00:29:24 +0300

    Initial commit

---
+++ src/main/java/ru/endlesscode/mimic/Mimic.java
@@ -0,0 +1,13 @@
+package ru.endlesscode.mimic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main Plugin class
+ *
+ * @author Osip Fatkullin
+ * @since 1.0
+ */
+public class Mimic extends JavaPlugin {
+    
+}
