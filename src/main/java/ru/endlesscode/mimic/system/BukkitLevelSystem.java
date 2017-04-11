ef1f3b84577bbf456dd6612eb8607af1d2ee023a
OsipXD <osip.fatkullin@gmail.com>
2017-04-11 23:03:50 +0300

    Updated copyright

--- src/main/java/ru/endlesscode/mimic/system/BukkitLevelSystem.java
+++ src/main/java/ru/endlesscode/mimic/system/BukkitLevelSystem.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of BukkitMimic.
- * Copyright (C) 2017 osipf
+ * Copyright (C) 2017 Osip Fatkullin
  *
  * BukkitMimic is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
